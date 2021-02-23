de0b41092c56f74eae102f1e1ae221e52854f7fc
Uladzimir86 <olodja@rambler.ru>
2021-02-23 13:29:58 +0300

    feat: add solution (no params passed) to src/index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
- 
-  let arr = matrix[0];
+  let  arr = [];
+     if (!Array.isArray(matrix)) return arr;
+
+  arr = matrix[0];
 
   for (i = 1; i<matrix.length; i++) {
     if (i%2 > 0) {
